2a008d5ee733b4c4df50949474256c6152253704
David Saffo <dsaffo@luc.edu>
2017-11-17 10:08:24 -0600

    first draft of site no styling just components

---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    
+    $('.carousel').carousel();
+    
+    $('.modal').modal();
+    
+    $('.button-collapse').sideNav();
+    
+    
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
