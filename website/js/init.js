b52091349f69a2d65714f77dcdb6fce55696d5c0
Anirrudh Krishnan <anik597@gmail.com>
2017-12-21 16:18:32 -0600

    Added Banner, Added Slider with Materialize, Added other webpages, added some other stuff. Still a lot of work that needs to be done.

---
+++ website/js/init.js
@@ -0,0 +1,17 @@
+(function($){
+  $(function(){
+
+
+    $('.carousel').carousel();
+
+    $('.modal').modal();
+
+    $('.button-collapse').sideNav();
+
+    $('.materialboxed').materialbox();
+
+    $('.slider').slider();
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
